839c41f4d8d9596cbf26722854f0064c41f07e49
mariliatd <mariliatd@gmail.com>
2022-07-26 20:40:59 -0300

    Add Point2d functions

--- app/src/main/kotlin/galaxyraiders/core/physics/Point2D.kt
+++ app/src/main/kotlin/galaxyraiders/core/physics/Point2D.kt
@@ -1,13 +1,15 @@
 @file:Suppress("UNUSED_PARAMETER") // <- REMOVE
 package galaxyraiders.core.physics
 
+import kotlin.math.sqrt
+
 data class Point2D(val x: Double, val y: Double) {
   operator fun plus(p: Point2D): Point2D {
-    return INVALID_POINT
+    return Point2D(this.x + p.x, this.y + p.y)
   }
 
   operator fun plus(v: Vector2D): Point2D {
-    return INVALID_POINT
+    return Point2D(this.x + v.dx, this.y + v.dy)
   }
 
   override fun toString(): String {
@@ -15,26 +17,30 @@ data class Point2D(val x: Double, val y: Double) {
   }
 
   fun toVector(): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(this.x, this.y)
   }
 
   fun impactVector(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    val dx = this.x - p.x
+    val dy = this.y - p.y
+    return Vector2D(sqrt(dx * dx), sqrt(dy * dy))
   }
 
   fun impactDirection(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return this.impactVector(p).unit
   }
 
   fun contactVector(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return this.impactVector(p).normal
   }
 
   fun contactDirection(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return this.impactDirection(p).normal
   }
 
   fun distance(p: Point2D): Double {
-    return INVALID_DOUBLE
+    val dx = this.x - p.x
+    val dy = this.y - p.y
+    return sqrt(dx * dx + dy * dy)
   }
 }
